bc92583f56c1633a01887fc707be134759c8830d
Lance Hamic <lsh14@my.fsu.edu>
2017-11-09 03:35:38 +0000

    Lesson 6 final commit

--- README.md
+++ README.md
@@ -4,19 +4,18 @@
 Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+(November 3, 2017)
 
 ### Student Name:  
-(First and Last Name)
+(Lance Hamic)
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+(The purpose of this assignment was to develop and design a photo gallery application and learn javascript while doing it.)
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+(https://lsh14.github.io/lesson6_javascript3/)
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. (A Browser object model (BOM) is a hierarchy of objects that provides programmatic access to a different aspect of the web browser window or the web page.)
+2. (Dynamic HTML is an interaction that can change content of the web page without reloading. It is a combination of HTML, CSS, and JavaScript.)
+3. (The location object allows changes to a new web page from within JavaScript code. Location object properties allow modification of URL individual portions.)
--- photos.js
+++ photos.js
@@ -1,11 +1,9 @@
 /*    
     Program Name:  Photo Gallery Application
-    Author: 
-    Date:   
+    Author: Lance Hamic
+    Date:   11/8/2017
     Filename: photos.js
-    
  */
-
 
 "use strict"; // interpret document contents in JavaScript strict mode
 
@@ -13,98 +11,142 @@
 var photoOrder = [1,2,3,4,5];
 var figureCount = 3;
 
-/*****************************************populateFigures() FUNCTION*************************************/
-/* This method adds src values to img elements based on order specified in photoOrder array             */
-/********************************************************************************************************/
 function populateFigures() {
+   var filename;
+   var currentFig;
+   
+if (figureCount === 3) {
+
+   for (var i = 1; i < 4; i++) {
+      filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+      currentFig = document.getElementsByTagName("img")[i - 1];
+      currentFig.src = filename;
+   }// end of for loop
+   }  else {
+      for (var i = 0; i < 5; i++) {
+       filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+      currentFig = document.getElementsByTagName("img")[i];
+      currentFig.src = filename;
+      }
+   }
+   
+   
+
+}
 
 
 
 
-}//end of populateFigures function
-
-/**********************************END OF populateFigures() FUNCTION*************************************/
-
-
-
-/*********************************rightArrow() FUNCTION***************************************************/
-/* This method shifts all images one figure to the right, and change values in photoOrder array to match */
-/********************************************************************************************************/
+/* shift all images one figure to the right, and change values in photoOrder array to match  */
 function rightArrow() {
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] + 1) === 6) {
          photoOrder[i] = 1;
       } else {
          photoOrder[i] += 1;
-      }//end of else
-      
+      }
       populateFigures();
-      
    }//end of for loop
 }//end of rightArrow Function
 
-/***********************************END OF RIGHT ARROW FUNCTION******************************************/
 
-/********************************leftArrow() Function****************************************************/
-/*This method shifts all images one figure to the left, and change values in photoOrder array to match  */
-/********************************************************************************************************/
+
+
+
+/* shift all images one figure to the left, and change values in photoOrder array to match  */
 function leftArrow() {
-    for (var i = 0; i < 5; i++) {
-        if ((photoOrder[i] - 1) === 0) {
-            photoOrder[i] = 5;
-        } else {
-            photoOrder[i] -= 1;
-        }//end of else
-        
+   for (var i = 0; i < 5; i++) {
+      if ((photoOrder[i] - 1) === 0) {
+         photoOrder[i] = 5;
+      } else {
+         photoOrder[i] -= 1;
+      }
       populateFigures();
-      
-    }//end of for loop
+   }//end of for loop
 }//end of leftArrow Function
 
-/****************************END OF LEFT ARROW FUNCTION*************************************************/
-
-
-/***************************************previewFive() Function******************************************/
-/*           This method is executed to allow the photo gallery to shift to five image layout.         */
-/******************************************************************************************************/
 function previewFive() {
    
+   var articleEl = document.getElementsByTagName("article")[0];
+   
+   var lastFigure = document.createElement("figure");
+   
+   lastFigure.id = "fig5";
+   lastFigure.style.zIndex = "5";
+   lastFigure.style.position = "absolute";
+   lastFigure.style.right - "45px";
+   lastFigure.style.top = "67px";
+   
+   var lastImage = document.createElement("img");
+   lastImage.width = "240";
+   lastImage.height = "135";
+   
+   lastFigure.appendChild(lastImage);
+   
+   articleEl.appendChild(lastFigure);
+   
+   var firstFigure = lastFigure.cloneNode(true);
+   
+   firstFigure.id = "fig1";
+   firstFigure.style.right = " ";
+   firstFigure.style.left = "45px";
+   
+   articleEl.insertBefore(firstFigure, document.getElementById("fig2"));
+   
+      document.getElementsByTagName("img")[0].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+      document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
 
+  figureCount = 5;
+  
+  var numberButton = document.querySelector("#fiveButton p");
+  numberButton.removeEventListener("click", previewFive, false);
    
    
-}//end of previewFive()
+   
+   
+}
 
-/****************************************END of previewFive() Function**********************************/
 
-
-
-/***********************************createEventListeners() Function*************************************/
-/*         This method attaches event listeners to the buttons (left, right, and show all).            */
-/*******************************************************************************************************/
 function createEventListeners() {
+   var leftarrow = document.getElementById("leftarrow");
+      if (leftarrow.addEventListener) {
+         leftarrow.addEventListener("click", leftArrow, false);
+      }
+      else if (leftarrow.attachEvent) {
+         leftarrow.attachEvent("onclick", leftArrow);
+      }// end of else if
+      
+   var rightarrow = document.getElementById("rightarrow");
+      if (rightarrow.addEventListener) {
+          rightarrow.addEventListener("click", rightArrow, false);
+      } else if (rightarrow.attachEvent) {
+   rightarrow.attachEvent("onclick", rightArrow);
+} // end of else if
+
    
+   var showAllButton = document.querySelector("#fiveButton p");
+      if (showAllButton.addEventListener) {
+         showAllButton.addEventListener("click", previewFive, false);
+         }  else if (showAllButton.attachEvent) {
+            showAllButton.attachEvent("onclick", previewFive);
+         }
+      
+}
 
-
-   
-}//end of createEventListeners function
-
-/************************************END OF createEventListeners() FUNCTION****************************/
-
-
-/************************************setUpPage() FUNCTION**********************************************/
 /* create event listeners and populate image elements */
 function setUpPage() {
    createEventListeners();
    populateFigures();
 }//end of setUpPage Function
 
-/***********************************END OF setUpPage() FUNCTION***************************************/
+
+
 
 
 
 /* run setUpPage() function when page finishes loading */
 if (window.addEventListener) {
-      window.addEventListener("load", setUpPage, false); 
+  window.addEventListener("load", setUpPage, false); 
 } else if (window.attachEvent)  {
-      window.attachEvent("onload", setUpPage);
-}//end of else if
+  window.attachEvent("onload", setUpPage);
+}
